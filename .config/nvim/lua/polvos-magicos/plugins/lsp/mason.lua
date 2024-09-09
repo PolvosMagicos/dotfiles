09723693bc70699968f75757f2279c873fcccfd0
PolvosMagicos <73202603+PolvosMagicos@users.noreply.github.com>
2024-09-09 00:37:48 -0500

    fix: ts lsp name updated

--- .config/nvim/lua/polvos-magicos/plugins/lsp/mason.lua
+++ .config/nvim/lua/polvos-magicos/plugins/lsp/mason.lua
@@ -12,7 +12,7 @@ return {
 		},
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "clangd", "jdtls", "tsserver", "rust_analyzer", "pylsp" },
+				ensure_installed = { "lua_ls", "clangd", "jdtls", "ts_ls", "rust_analyzer", "pylsp" },
 			})
 		end,
 	},
