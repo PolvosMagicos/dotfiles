034e385d7d714162fb9ccfdba5549c53b05afa0f
Polvos Magicos <73202603+PolvosMagicos@users.noreply.github.com>
2025-01-05 15:49:02 -0500

    fix(nvim): fix K behaviour in visual mode

--- .config/nvim/lua/polvos-magicos/plugins/better-escape.lua
+++ .config/nvim/lua/polvos-magicos/plugins/better-escape.lua
@@ -1,6 +1,38 @@
 return {
   "max397574/better-escape.nvim",
   config = function()
-    require("better_escape").setup()
+    require("better_escape").setup {
+      timeout = vim.o.timeoutlen,
+      default_mappings = false,
+      mappings = {
+        i = {
+          j = {
+            k = "<Esc>",
+            j = "<Esc>",
+          },
+        },
+        c = {
+          j = {
+            k = "<Esc>",
+            j = "<Esc>",
+          },
+        },
+        t = {
+          j = {
+            k = "<C-\\><C-n>",
+          },
+        },
+        -- v = {
+        --     j = {
+        --         k = "<Esc>",
+        --     },
+        -- },
+        s = {
+          j = {
+            k = "<Esc>",
+          },
+        },
+      },
+    }
   end,
 }
