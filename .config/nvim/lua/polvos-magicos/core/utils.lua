2ff84e925e713977525be59fa35b3586aa75a89d
Polvos Magicos <73202603+PolvosMagicos@users.noreply.github.com>
2025-02-01 17:57:23 -0500

    feat(nvim): commands description

--- .config/nvim/lua/polvos-magicos/core/utils.lua
+++ .config/nvim/lua/polvos-magicos/core/utils.lua
@@ -31,13 +31,23 @@ M.on_attach = function(client, bufnr)
 	local augroup = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
 	-- Set up keymaps (shared across all servers)
 	local opts = { noremap = true, silent = true, buffer = bufnr }
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-	vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
-	vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
-	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", opts, { desc = "Show Hover Information" }))
+	vim.keymap.set(
+		"n",
+		"<leader>gd",
+		vim.lsp.buf.definition,
+		vim.tbl_extend("force", opts, { desc = "Go to Definition" })
+	)
+	vim.keymap.set(
+		"n",
+		"<leader>gr",
+		vim.lsp.buf.references,
+		vim.tbl_extend("force", opts, { desc = "List References" })
+	)
+	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "Code Action" }))
 	vim.keymap.set("n", "<leader>lh", function()
 		vim.lsp.inlay_hint(bufnr, not vim.lsp.inlay_hint.is_enabled(bufnr))
-	end, opts)
+	end, vim.tbl_extend("force", opts, { desc = "Toggle Inlay Hints" }))
 
 	-- Enable formatting on save (if supported by the server)
 	if client.supports_method("textDocument/formatting") then
