96baca545851fcb98d33c43aca8fad49d34864f5
Polvos Magicos <73202603+PolvosMagicos@users.noreply.github.com>
2025-02-01 17:58:14 -0500

    feat(nvim): git-signs && mini plugins

--- .config/nvim/lua/polvos-magicos/plugins/mini.lua
+++ .config/nvim/lua/polvos-magicos/plugins/mini.lua
@@ -1,12 +1,14 @@
 return {
-  "echasnovski/mini.nvim",
-  version = false,
-  enabled = true,
-  lazy = false,
-  config = function()
-    require("mini.splitjoin").setup()
-    require("mini.surround").setup({ silent = true })
-    require("mini.move").setup()
-    require("mini.cursorword").setup()
-  end,
+	"echasnovski/mini.nvim",
+	version = false,
+	enabled = true,
+	lazy = false,
+	config = function()
+		require("mini.ai").setup()
+		require("mini.splitjoin").setup()
+		require("mini.operators").setup()
+		require("mini.surround").setup()
+		require("mini.move").setup()
+		require("mini.cursorword").setup()
+	end,
 }
