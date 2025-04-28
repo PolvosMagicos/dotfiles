945bbc7a85b99dc1f1c3d8ff47e9ac41cf613d68
Polvos Magicos <73202603+PolvosMagicos@users.noreply.github.com>
2025-04-27 20:47:51 -0400

    feat(nvim): migrate from nvim-cmp to blin-cmp

---
+++ .config/nvim/lua/polvos-magicos/plugins/archived/completions.lua
@@ -0,0 +1,85 @@
+local has_words_before = function()
+	if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+		return false
+	end
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+end
+
+return {
+	{
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"onsails/lspkind.nvim",
+		},
+		config = function()
+			local cmp = require("cmp")
+			require("luasnip.loaders.from_vscode").lazy_load()
+			require("lspkind").init()
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+
+				-- Mappings
+				mapping = cmp.mapping.preset.insert({
+					-- Keymaps for use tab and don't have conflict with command mode
+					["<Tab>"] = vim.schedule_wrap(function(fallback)
+						if cmp.visible() and has_words_before() then
+							cmp.select_next_item({ behavior = cmp.SelectBehavior })
+						elseif has_words_before() then
+							cmp.complete()
+						else
+							fallback()
+						end
+					end),
+
+					["<S-Tab>"] = vim.schedule_wrap(function(fallback)
+						if cmp.visible() and has_words_before() then
+							cmp.select_prev_item({ behavior = cmp.SelectBehavior })
+						else
+							fallback()
+						end
+					end),
+
+					["<C-Space>"] = cmp.mapping.complete(),
+
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+
+				-- Sources
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" }, -- For luasnip users.
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+			-- Cargo cmp lazily
+			--[[ vim.api.nvim_create_autocmd("BufRead", {
+        group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
+        pattern = "Cargo.toml",
+        callback = function()
+          cmp.setup.buffer({ sources = { { name = "crates" } } })
+        end,
+      }) ]]
+		end,
+	},
+}
