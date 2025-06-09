b0667102cb9ea0da2121eda2ecca1af6e5edd069
Polvos Magicos <73202603+PolvosMagicos@users.noreply.github.com>
2025-06-09 14:31:54 -0400

    feat(nvim): fix duplicate rust analizer lsp

--- .config/nvim/lua/polvos-magicos/plugins/lsp/mason.lua
+++ .config/nvim/lua/polvos-magicos/plugins/lsp/mason.lua
@@ -1,40 +1,41 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup({
-        ui = {
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
-          },
-        },
-      })
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {
-      auto_install = true,
-    },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",        -- Lua
-          "clangd",        -- C/C++
-          "jdtls",         -- Java
-          "rust_analyzer", -- Rust
-          "pylsp",         -- Python
-          "tailwindcss",   -- Tailwind CSS
-          "html",          -- HTML
-          "marksman",      -- Markdown
-        },
-        automatic_installation = {
-          exclude = { "ts_ls" },
-        },
-        auto_install = true,
-      })
-    end,
-  },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			auto_install = true,
+		},
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls", -- Lua
+					"clangd", -- C/C++
+					"jdtls", -- Java
+					"pylsp", -- Python
+					"tailwindcss", -- Tailwind CSS
+					"html", -- HTML
+					"marksman", -- Markdown
+				},
+				automatic_installation = {
+					-- typescritp tools handles ts_ls
+					-- rustaceanvim needs `rustup component add rust-analyzer`
+					exclude = { "ts_ls", "rust_analyzer" },
+				},
+				auto_install = true,
+			})
+		end,
+	},
 }
