f419f773b97b85a76cd626a54137a73f32d61737
Polvos Magicos <73202603+PolvosMagicos@users.noreply.github.com>
2025-06-08 21:36:11 -0400

    fea(nvim): remove & exclude ts_ls lsp

--- .config/nvim/lua/polvos-magicos/plugins/lsp/mason.lua
+++ .config/nvim/lua/polvos-magicos/plugins/lsp/mason.lua
@@ -1,19 +1,19 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup({
-				ui = {
-					icons = {
-						package_installed = "✓",
-						package_pending = "➜",
-						package_uninstalled = "✗",
-					},
-				},
-			})
-		end,
-	},
-{
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      })
+    end,
+  },
+  {
     "williamboman/mason-lspconfig.nvim",
     opts = {
       auto_install = true,
@@ -21,16 +21,19 @@ return {
     config = function()
       require("mason-lspconfig").setup({
         ensure_installed = {
-          "lua_ls",       -- Lua
-          "clangd",       -- C/C++
-          "jdtls",        -- Java
-          "rust_analyzer",-- Rust
-          "pylsp",        -- Python
-          "tailwindcss",  -- Tailwind CSS
-          "html",         -- HTML
-          "marksman",     -- Markdown
+          "lua_ls",        -- Lua
+          "clangd",        -- C/C++
+          "jdtls",         -- Java
+          "rust_analyzer", -- Rust
+          "pylsp",         -- Python
+          "tailwindcss",   -- Tailwind CSS
+          "html",          -- HTML
+          "marksman",      -- Markdown
         },
-        automatic_installation = true, -- automatically detect which servers to install
+        automatic_installation = {
+          exclude = { "ts_ls" },
+        },
+        auto_install = true,
       })
     end,
   },
