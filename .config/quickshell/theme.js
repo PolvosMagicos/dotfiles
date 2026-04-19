f38c78410dffca2ce9d444a5df83f1a33d481ff5
Polvos Magicos <73202603+PolvosMagicos@users.noreply.github.com>
2026-04-19 01:56:11 -0500

    feat(quickshell): catpuccin theme

---
+++ .config/quickshell/theme.js
@@ -0,0 +1,31 @@
+.pragma library
+
+// Catppuccin Mocha
+var rosewater = "#f5e0dc"
+var flamingo  = "#f2cdcd"
+var pink      = "#f5c2e7"
+var mauve     = "#cba6f7"
+var red       = "#f38ba8"
+var maroon    = "#eba0ac"
+var peach     = "#fab387"
+var yellow    = "#f9e2af"
+var green     = "#a6e3a1"
+var teal      = "#94e2d5"
+var sky       = "#89dceb"
+var sapphire  = "#74c7ec"
+var blue      = "#89b4fa"
+var lavender  = "#b4befe"
+
+var text      = "#cdd6f4"
+var subtext1  = "#bac2de"
+var subtext0  = "#a6adc8"
+var overlay2  = "#9399b2"
+var overlay1  = "#7f849c"
+var overlay0  = "#6c7086"
+
+var surface2  = "#585b70"
+var surface1  = "#45475a"
+var surface0  = "#313244"
+var base      = "#1e1e2e"
+var mantle    = "#181825"
+var crust     = "#11111b"
